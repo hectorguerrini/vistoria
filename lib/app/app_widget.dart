4f0ea9fe10c3ab0b3854e109924e75d4eab8ff1a
Hector Guerrini Herrera <hectorumaga@hotmail.com>
2020-10-26 14:29:09 -0300

    app widget

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:oktoast/oktoast.dart';
-import 'package:vistoria/app/shared/splash/splash_screen.dart';
 
 class AppWidget extends StatelessWidget {
   final Future<FirebaseApp> _initialization = Firebase.initializeApp();
@@ -27,10 +26,11 @@ class AppWidget extends StatelessWidget {
             );
           }
 
-          return Center(
-            child: Container(
-                decoration: BoxDecoration(color: Colors.white),
-                child: CircularProgressIndicator()),
+          return MaterialApp(
+            home: Scaffold(
+                body: Container(
+                    decoration: BoxDecoration(color: Colors.white),
+                    child: CircularProgressIndicator())),
           );
         });
   }
